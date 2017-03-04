064ef1027f71bcd43309a74d6dd7873ed8809a11
CosmicCandi <kalea.wolff@gmail.com>
2017-03-04 11:24:22 -0500

    Moved root endpoint to /users. Will get a list of all users. Will be useful for displaying potential people to follow.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #User Endpoints
-  root 'users#index'
+  get 'users' => 'users#index'
   post 'create' => 'users#create'
   post 'login' => 'sessions#login'
 
