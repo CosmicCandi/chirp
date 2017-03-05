cf269abe708060e0c17c68ee2c08612bafd70a72
CosmicCandi <kalea.wolff@gmail.com>
2017-03-05 15:45:23 -0500

    Backed out self-written pagination method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,36 +2,6 @@ class PostsController < ApplicationController
 
   before_action :require_user
 
-  def paginate
-    #Pseudo Code for Pagination
-    @chirps = Post.timeline(paginate_params, current_user)
-    paginate = nil
-    unless paginate == nil
-      render json: @chirps
-    else
-      until @chirps.count / params[:limit] <= 0 do
-        @chirps.collect limit(params[:limit])
-
-      end
-    # Start the rendering of the pagination
-    #  Get the limit from the user
-    #    @limit = params[:limit]
-    #  Get the offset from the user
-    #    @offset = params[:offset]
-    #  Run calculations for offset by page
-    #  @offset = (params[:page] - 1 * params[:offset])
-    #  render json: pagination
-
-    @chirps = Post.timeline(paginate_params, current_user)
-    @chirps.count
-    @offset = (params[:page] - 1) * params[:offset]
-
-    render json: @chirps
-    @chirps.limit(params[:offset])
-
-
-  end
-
   def index
     if current_user
       @chirps = Post.timeline(current_user)
@@ -55,9 +25,5 @@ class PostsController < ApplicationController
   def chirp_params
     params.permit(:body)
   end
-
-  def paginate_params
-    params.permit(:pagination,:page, :offset, :limit)
-  end
-
+  
 end
