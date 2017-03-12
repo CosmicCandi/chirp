b06526353e2d00b6bb85b75a5a1fc3fd5a5c7540
CosmicCandi <kalea.wolff@gmail.com>
2017-03-11 20:02:33 -0500

    Redefined avatar to image to see if it will stop throwing errors on the F/E side.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ has_many :posts
 validates :username, :first_name, :last_name, presence: true
 validates :username, uniqueness: true
 
-  def avatar
+  def image
     image.blank? ? "https://raw.githubusercontent.com/RagenS9/chirp/3969a24a79e0fda25b3dd3ceaf71f4edc3a2af78/img/placeholder.JPG" : image
   end
 
